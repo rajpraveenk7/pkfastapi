f0e2c3f29d7e8f74e726065e7f05504ac2bd2c2b
rajpraveenk7 <rajpraveenk7@gmail.com>
2024-09-24 21:59:47 +0530

    1 fastapi

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+app=FastAPI(title="MY API")
+
+items=[]
+ 
+@app.get("/")
+def root():
+    return {"Hello":"World"}
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/items")
+def list_items(limit: int=10):
+    return items[0:limit]
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+    
+    #https://youtu.be/iWS9ogMPOI0?si=_JmjwpA2hu8077eH
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+this fast api
